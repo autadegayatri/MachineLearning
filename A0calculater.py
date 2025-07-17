24ec58f25d0402f728cb466dc10289d3b3c2852d
Gayatri Balasaheb Autade <143700029+autadegayatri@users.noreply.github.com>
2025-07-17 13:09:43 +0530

    Add files via upload

---
+++ A0calculater.py
@@ -0,0 +1,11 @@
+x=int(input("enter the 1st no: "))
+y=int(input("enter the 1st no: "))
+op=int(input("enter which operation you want to perform:/n 1)Add/n 2)subs/n3)mul/n4)div"))
+if op==1:
+    print("The sum is ",x+y)
+elif op==2:
+    print("The difference is ",x-y)
+elif op==3:
+    print("The product is ",x*y)
+else:
+    print("The division is ",x/y)
